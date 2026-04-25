2bac6d09241141b2efe0034b9f663d8f169dfa54
rajukumarpothuraju <rajukumarpothuraju@2004gmail.com>
2026-04-25 12:28:28 +0530

    fixed payment controller import case sensitivity

--- backend/routes/payment-routes.js
+++ backend/routes/payment-routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   createOrder,
   verifyPayment,
-} from "../controllers/paymentController.js";
+} from "../controllers/paymentcontroller.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
